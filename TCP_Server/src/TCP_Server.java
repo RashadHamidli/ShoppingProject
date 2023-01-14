2bb66a83c5516681eb2cc271ac3f0c9261a86f7d
RashadHamidli <mr_rashad@email.com>
2023-01-14 11:21:21 +0400

    check

--- TCP_Server/src/TCP_Server.java
+++ TCP_Server/src/TCP_Server.java
@@ -5,11 +5,7 @@ public class TCP_Server {
         System.out.println("add intellij");
         System.out.println("null");
         System.out.println("new test");
-<<<<<<< HEAD
         System.out.println("mmmmmmmmmmmmm");
-=======
         System.out.println("asdas");
->>>>>>> 92e43f5155c9329a2318b9de608e17b13a504c58
-        
     }
 }
