c8536921c0cb05b3ce7a23534c6aa69237e9ef91
RashadHamidli <116068294+RashadHamidli@users.noreply.github.com>
2023-01-14 10:45:01 +0400

    Update TCP_Server.java
    add change

--- TCP_Server/src/TCP_Server.java
+++ TCP_Server/src/TCP_Server.java
@@ -1,5 +1,6 @@
 public class TCP_Server {
     public static void main(String[] args) {
         System.out.println("git add tcp server");
+        System.out.println("Check add new change");
     }
 }
