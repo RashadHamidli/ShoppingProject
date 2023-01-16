bbf8f3b44f68304b6d5039a1a7415e5f611cba0c
RashadHamidli <mr_rashad@email.com>
2023-01-16 09:17:16 +0400

    test

--- TCP_Server/src/TCP_Client.java
+++ TCP_Server/src/TCP_Client.java
@@ -1,5 +1,15 @@
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class TCP_Client {
-    public static void main(String[] args) {
-        System.out.println("add git tcp client");
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 55555);
+        OutputStream outputStream= socket.getOutputStream();
+        DataOutputStream dataOutputStream= new DataOutputStream(outputStream);
+        dataOutputStream.write("test".getBytes(StandardCharsets.UTF_8));
+        socket.close();
     }
 }
