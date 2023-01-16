79f0009a4776748bc4f9ecdd0c2ddf9e55d8102b
RashadHamidli <mr_rashad@email.com>
2023-01-16 09:02:35 +0400

    add new server class

--- TCP_Server/src/TCP_Server.java
+++ TCP_Server/src/TCP_Server.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCP_Server {
-    public static void main(String[] args) {
-        System.out.println("git add tcp server");
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket=new ServerSocket(55555);
+        while (true){
+            System.out.println("gozlemeye basladi");
+            Socket connection =serverSocket.accept();
+            System.out.println("gozleyir");
+            InputStream inputStream=connection.getInputStream();
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+            BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+
+            String message =bufferedReader.readLine();
+            System.out.println("message from client="+message);
+        }
+
     }
 }
