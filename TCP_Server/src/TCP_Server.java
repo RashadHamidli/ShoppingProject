5e7c5ba4f02cb0b4ee367153357412ad4db5c444
RashadHamidli <mr_rashad@email.com>
2023-01-16 08:29:59 +0400

    add

--- TCP_Server/src/TCP_Server.java
+++ TCP_Server/src/TCP_Server.java
@@ -1,11 +1,5 @@
 public class TCP_Server {
     public static void main(String[] args) {
         System.out.println("git add tcp server");
-        System.out.println("Check add new change");
-        System.out.println("add intellij");
-        System.out.println("null");
-        System.out.println("new test");
-        System.out.println("mmmmmmmmmmmmm");
-        System.out.println("asdas");
     }
 }
